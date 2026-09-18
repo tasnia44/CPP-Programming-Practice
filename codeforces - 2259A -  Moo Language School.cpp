023f11c9c10ea4b8df858c18016e6cd148539445
Tasnia Farzana <tasniafarzana970@gmail.com>
2026-09-18 19:36:25 +0600

    Add files via upload

---
+++ codeforces - 2259A -  Moo Language School.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n, k;
+        cin >> n >> k;
+        string s;
+        cin >> s;
+        int ans = 0;
+        for(int i = 0; i < n; i += k)
+        {
+            bool allOne = true;
+            for(int j = i; j < i + k; j++)
+            {
+                if(s[j] == '0')
+                {
+                    allOne = false;
+                    break;
+                }
+            }
+                if(allOne)
+                {
+                    ans++;
+                }
+            }
+            cout << ans << endl;
+        }
+    return 0;
+}
